bd22c561f3acb4272789f2a695fd99ea68a80d3a
northp <realblessingchitra@gmail.com>
2022-09-25 14:04:58 +0200

    tests and bugs

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+gem 'github-pages', group: :jekyll_plugins
+gem "jekyll-theme-minimal"
+gem "rspec"
+
+gem "webrick", "~> 1.7"
--- db.c
+++ db.c
@@ -27,6 +27,8 @@ typedef enum {
 typedef enum {
 
     PREPARE_SUCCESS,
+    PREPARE_NEGATIVE_ID,
+    PREPARE_STRING_TOO_LONG,
     PREPARE_SYNTAX_ERROR,
     PREPARE_UNRECOGNIZED_STATEMENT,
     
@@ -59,8 +61,8 @@ typedef struct
 {
 
     uint32_t    id;
-    char        username[COLUMN_USERNAME_SIZE];
-    char        email[COLUMN_EMAIL_SIZE];
+    char        username[COLUMN_USERNAME_SIZE + 1];
+    char        email[COLUMN_EMAIL_SIZE + 1];
 
 } Row;
 
@@ -102,7 +104,7 @@ typedef struct
 void
 print_row(Row* row)
 {
-    printf("%d, %s, %s\n", row->id, row->username, row->email);
+    printf("(%d, %s, %s)\n", row->id, row->username, row->email);
     return;
 }
 
@@ -180,21 +182,46 @@ do_meta_command(InputBuffer* input_buffer)
     }    
 }
 
+PrepareResult
+prepare_insert(InputBuffer* input_buffer, Statement* statement)
+{
+    statement->type = STATEMENT_INSERT;
+
+    char* keyword = strtok(input_buffer->buffer, " ");
+    char* id_string = strtok(NULL, " ");
+    char* username = strtok(NULL, " ");
+    char* email = strtok(NULL, " ");
+
+    if (id_string == NULL || username == NULL || email == NULL){
+        return PREPARE_SYNTAX_ERROR;
+    }
+
+    int id = atoi(id_string);
+    if(id < 0){
+        return PREPARE_NEGATIVE_ID;
+    }
+
+    if(strlen(username) > COLUMN_USERNAME_SIZE){
+        return PREPARE_STRING_TOO_LONG;
+    }
+
+    if(strlen(email) > COLUMN_EMAIL_SIZE){
+        return PREPARE_STRING_TOO_LONG;
+    }
+
+    statement->row_to_insert.id = id;
+    strcpy(statement->row_to_insert.username, username);
+    strcpy(statement->row_to_insert.email, email);
+
+    return PREPARE_SUCCESS;
+
+}
 
 PrepareResult 
 prepare_statement(InputBuffer* input_buffer, Statement* statement)
 {
     if(strncmp(input_buffer->buffer, "insert", 6) == 0){
-        statement->type = STATEMENT_INSERT;
-        int args_assigned = sscanf(
-            input_buffer->buffer, "insert %d %s %s", &(statement->row_to_insert.id),
-            statement->row_to_insert.username, statement->row_to_insert.email
-        );
-
-        if(args_assigned < 3){
-            return PREPARE_SYNTAX_ERROR;
-        }
-        return PREPARE_SUCCESS;
+        return prepare_insert(input_buffer, statement);
     }
     
     if(strcmp(input_buffer->buffer, "select") == 0){
@@ -204,9 +231,6 @@ prepare_statement(InputBuffer* input_buffer, Statement* statement)
 
     return PREPARE_UNRECOGNIZED_STATEMENT;
 }
-
-
-
 
 ExecuteResult 
 execute_insert(Statement* statement, Table* table)
@@ -321,8 +345,14 @@ main(int argc, char* argv[])
 
         Statement statement;
         switch (prepare_statement(input_buffer, &statement)){
-            case(PREPARE_SUCCESS):
+            case (PREPARE_SUCCESS):
                 break;
+            case (PREPARE_NEGATIVE_ID):
+                printf("ID must be positive.\n");
+                continue;
+            case (PREPARE_STRING_TOO_LONG):
+                printf("String is too long.\n");
+                continue;
             case (PREPARE_SYNTAX_ERROR):
                 printf("Syntax Error. Could not parse statement.\n");
                 continue;
@@ -333,7 +363,7 @@ main(int argc, char* argv[])
 
         switch (execute_statement(&statement, table)){
             case (EXECUTE_SUCCESS):
-                printf("Executed\n");
+                printf("Executed.\n");
                 break;
             case (EXECUTE_TABLE_FULL):
                 printf("Error: Table full.\n");
